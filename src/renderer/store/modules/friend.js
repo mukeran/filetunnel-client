7b6b751cb429822d73ccdf13a28e9d57bc17e7dc
Miracle-Messi <wuyf@bupt.edu.cn>
2020-07-02 23:18:42 +0800

    :bulb: Add comments in source code

--- src/renderer/store/modules/friend.js
+++ src/renderer/store/modules/friend.js
@@ -4,12 +4,27 @@ const state = {
 }
 
 const mutations = {
+  /**
+   * Update friend list
+   * @param {Object} state Data storage
+   * @param {List} param1 Friend list
+   */
   updateFriendList (state, { friends }) {
     state.friends = friends
   },
+  /**
+   * Update friend requests
+   * @param {Object} state Data storage
+   * @param {List} param1 Friend requests
+   */
   updateFriendRequests (state, { friendRequests }) {
     state.friendRequests = friendRequests
   },
+  /**
+   * Remove friend request
+   * @param {Object} state Data storage
+   * @param {String} _id Request _id
+   */
   removeFriendRequest (state, _id) {
     state.friendRequests = state.friendRequests.filter(friendRequest => friendRequest._id !== _id)
   }
