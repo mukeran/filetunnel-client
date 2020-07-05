8bb5e134551c4c6a0f1614b60b3b44bd66d43be9
Miracle-Messi <wuyf@bupt.edu.cn>
2020-07-05 17:05:34 +0800

    :bulb: Add some comments in source code

--- src/client/response/friend.js
+++ src/client/response/friend.js
@@ -2,6 +2,10 @@ import store from '../../renderer/store'
 import { sendResponse } from '../index'
 import status from '../status'
 
+/**
+ * Process friend requests
+ * @param {Object} packet Packet received
+ */
 export function sendFriendRequests (packet) {
   const { friendRequests } = packet.data
   store.dispatch('updateFriendRequests', { friendRequests })
--- src/client/response/offlineTransfer.js
+++ src/client/response/offlineTransfer.js
@@ -2,6 +2,10 @@ import store from '../../renderer/store'
 import { sendResponse } from '../index'
 import status from '../status'
 
+/**
+ * Process offline transfer requests
+ * @param {Oblect} packet Packet received
+ */
 export function sendOfflineTransfers (packet) {
   const { offlineTransfers } = packet.data
   store.dispatch('updateOfflineTransfers', { offlineTransfers })
